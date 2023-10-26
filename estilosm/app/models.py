85181dd93e405e7eb9765605d10ecfa8e1c3055f
G-Paludeto <143637399+G-Paludeto@users.noreply.github.com>
2023-10-26 08:59:05 -0300

    Add files via upload

---
+++ estilosm/app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+    
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+     
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    quantidade = models.IntegerField
+    peco = models.PositiveIntegerField
+    disponivel = models.BooleanField
+
+    def __str__(self):
+        return f'{self.marca} {self.nome}'
+
